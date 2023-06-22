157d96a11616e22df44b51accb6da48cceb38a59
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-06-22 19:58:35 +0100

    Ignore missing marks instead

--- instructor_unit_test_examples.py
+++ instructor_unit_test_examples.py
@@ -3,18 +3,17 @@ def red_or_blue(num):
 
 
 def average_exam_score(names_and_marks):
+    total_length = 0
     total_mark = 0
     for name_and_mark in names_and_marks:
         if 'mark' in name_and_mark:
             mark = name_and_mark['mark']
+            total_length = total_length + 1
             try:
                 total_mark = total_mark + mark
             except:
                 raise ValueError('Mark in not an integer.')
-
-        else:
-            total_mark = total_mark + 5
-    return total_mark / len(names_and_marks)
+    return total_mark /  total_length
 
 
 def increment_line_number():
