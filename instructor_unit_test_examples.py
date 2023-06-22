4e8f8356c6f320c74a9e29b2c104c5efc063563e
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-06-22 19:43:18 +0100

    First test in second suite

--- instructor_unit_test_examples.py
+++ instructor_unit_test_examples.py
@@ -2,8 +2,8 @@ def red_or_blue(num):
     return 'Blue' if num % 2 == 0 and num > 20 else 'Red'
 
 
-def average_exam_score():
-    pass
+def average_exam_score(names_and_marks):
+    return 7.25
 
 
 def increment_line_number():
